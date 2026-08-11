7f79d5741185ce1c19edf9dc98628f5ddce9f9b6
ImNothing <saifxg2@gmail.com>
2026-08-11 18:46:37 +0300

    fix bug

--- frontend/main.js
+++ frontend/main.js
@@ -4,17 +4,12 @@ let exp = document.querySelector('.experiment')
 
 
 if (window.localStorage.getItem('color')) {
-    console.log('yes');
     exp.style.backgroundColor = window.localStorage.getItem('color')
         lis.forEach((li) =>{
             li.classList.remove('active');
         })
-        document.querySelector(`[data-color='${window.localStorage.getItem('color')}']`).classList.add('active')
-}else {
-    console.log('no');
+    document.querySelector(`[data-color='${window.localStorage.getItem('color')}']`).classList.add('active')
 }
-
-
 
 
 lis.forEach((li)=>{
